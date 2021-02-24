107007906fc3c3d6f5c9f6cf7990320c469e24d0
Manuel Dugué <manuel.dugue@comvel.de>
2021-02-24 10:57:07 +0100

    using latest version of dependencies refactoring for parse-gedcom

--- src/worker.ts
+++ src/worker.ts
@@ -1,6 +1,6 @@
 import * as Comlink from "comlink"
 import { parse } from "date-fns"
-import parseGedcom from "parse-gedcom"
+import * as parseGedcom from "parse-gedcom"
 import { readFile } from "./readFile"
 
 const referenceDate = new Date()
@@ -21,37 +21,82 @@ export type Family = {
   marriage?: Date
 }
 
+type CompactIndividual = {
+  type: "INDI"
+  data?: {
+    "@FAMILY_SPOUSE"?: string
+    "+@FAMILY_SPOUSE"?: string[]
+    "BIRTH/DATE"?: string
+    "BIRTH/PLACE"?: string
+    "DEATH/DATE"?: string
+    "DEATH/PLACE"?: string
+    "EVENT/PLACE"?: string
+    "EVENT/TYPE"?: string
+    NAME: string
+    REFERENCE: string
+    SEX: "M" | "W"
+    formal_name: "INDIVIDUAL"
+    xref_id: string
+  }
+  children: Individual
+}
+
+type CompactFamily = {
+  type: "FAM"
+  data?: {
+    "+@CHILD"?: string[]
+    "+CHILD/_FREL"?: string[]
+    "+CHILD/_MREL"?: string[]
+    "@CHILD"?: string
+    "@HUSBAND"?: string
+    "@WIFE"?: string
+    "CHILD/_FREL"?: string
+    "CHILD/_MREL"?: string
+    "MARRIAGE/DATE"?: string
+    formal_name: "FAMILY"
+    xref_id: string
+  }
+  children: CompactFamily[]
+}
+
+type Parent = { children: (CompactIndividual | CompactFamily)[] }
+
+function notEmpty<TValue>(value: TValue | null | undefined): value is TValue {
+  return value != null
+}
+
 const dataWorker = {
   async loadData(file: Blob) {
     const gedcomData = await readFile(file)
     const data = parseGedcom.parse(gedcomData)
+    // @ts-expect-error
+    const compact: Parent = parseGedcom.compact(data) as Parent
 
-    const individuals: Individual[] = data
-      .filter((item) => item.tag === "INDI")
+    const individuals: Individual[] = compact.children
+      .filter((item): item is CompactIndividual => item.type === "INDI")
       .map((item) => {
-        const birth = item.tree
-          .find((node) => node.tag === "BIRT")
-          ?.tree.find((node) => node.tag === "DATE")?.data
-        const death = item.tree
-          .find((node) => node.tag === "DEAT")
-          ?.tree.find((node) => node.tag === "DATE")?.data
+        const { data } = item
+        if (data == null) return null
+        const birth = data["BIRTH/DATE"]
+        const death = data["DEATH/DATE"]
 
         return {
-          id: item.pointer.replace("@I", "").replace("@", ""),
-          name:
-            item.tree
-              .find((node) => node.tag === "NAME")
-              ?.data.replace(/\//g, "") || "no name",
+          id: data.xref_id.replace("@I", "").replace("@", ""),
+          name: data.NAME.replace(/\//g, "") || "no name",
           birth:
             birth != null ? parse(birth, "d LLL y", referenceDate) : undefined,
           death:
             death != null ? parse(death, "d LLL y", referenceDate) : undefined,
-          sex: item.tree.find((node) => node.tag === "SEX")?.data,
-          headOfFamilies: item.tree
-            .filter((node) => node.tag === "FAMS")
-            .map((node) => node.data.replace("@F", "").replace("@", "")),
+          sex: data.SEX,
+          headOfFamilies: [
+            data["@FAMILY_SPOUSE"],
+            ...(data["+@FAMILY_SPOUSE"] || []),
+          ]
+            .filter(notEmpty)
+            .map((node) => node.replace("@F", "").replace("@", "")),
         } as Individual
       })
+      .filter(notEmpty)
 
     const idToIndividualMap: {
       [id: string]: Individual
@@ -63,25 +108,26 @@ const dataWorker = {
       {}
     )
 
-    const families: Family[] = data
-      .filter((item) => item.tag === "FAM")
+    const families: Family[] = compact.children
+      .filter((item): item is CompactFamily => item.type === "FAM")
       .map((item) => {
-        const marriageDateString = item.tree
-          .find((node) => node.tag === "MARR")
-          ?.tree.find((node) => node.tag === "DATE")?.data
+        const { data } = item
+        if (data == null) return null
+        const marriageDateString = data["MARRIAGE/DATE"]
         return {
-          id: item.pointer.replace("@F", "").replace("@", ""),
-          children: item.tree
-            .filter((node) => node.tag === "CHIL")
-            .map((node) => node.data.replace("@I", "").replace("@", "")),
-          parents: item.tree
-            .filter((node) => node.tag === "HUSB" || node.tag === "WIFE")
-            .map((node) => node.data.replace("@I", "").replace("@", "")),
+          id: data.xref_id.replace("@F", "").replace("@", ""),
+          children: [data["@CHILD"], ...(data["+@CHILD"] || [])]
+            .filter(notEmpty)
+            .map((node) => node.replace("@I", "").replace("@", "")),
+          parents: [data["@HUSBAND"], data["@WIFE"]]
+            .filter(notEmpty)
+            .map((node) => node.replace("@I", "").replace("@", "")),
           marriage: marriageDateString
             ? new Date(marriageDateString)
             : undefined,
         }
       })
+      .filter(notEmpty)
     return {
       individuals,
       idToIndividualMap,
